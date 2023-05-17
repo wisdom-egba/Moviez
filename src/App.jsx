c932721d291d3ce5ee71935e486859a755f63fe0
Wisdom Egba <99222194+wiz-js@users.noreply.github.com>
2023-05-17 22:33:44 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
 import React from "react"
 import "./App.css"
-import { createBrowserRouter, createRoutesFromElements } from "react-router-dom"
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom"
+import { NavBar } from "./components/NavBar"
+import { Home } from "./components/Home"
 function App() {
-  const router = createBrowserRouter()
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+      </Route>
+    )
+  )
 
   return
 }
 
+const Root = () => {
+  return (
+    <>
+      <NavBar />
+      <div>
+        <Outlet />
+      </div>
+    </>
+  )
+}
 export default App
